f3b52202fd53160b9a878b96413e978bec6e4cb8
onkarJagtap2209 <onkarofficial2209@gmail.com>
2026-01-20 13:31:47 +0530

    Data Transformation Done

--- src/Components/data_ingestion.py
+++ src/Components/data_ingestion.py
@@ -7,6 +7,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -24,8 +27,10 @@ class DataIngestion:
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            # Creates artifacts/folder to store pipeline outputs.
 
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            # Keeps backup copy of original data.
 
             logging.info("Train test split initiated")
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
@@ -39,14 +44,19 @@ class DataIngestion:
             return(
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
+                # Gives file locations to next pipeline step.
 
             )
         except Exception as e:
             raise CustomException(e,sys)
+            #Shows clean error with file name & line number.
         
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
 
     
 
